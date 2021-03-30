db57bea7e343d80c14e2c0279362782c616f5298
Leo Clarke <leojclarke@gmail.com>
2021-03-30 12:40:43 +0200

    Initial commit to develop branch
    Includes basic typescript create react app setup with some basestyles applied

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="page-title">coding-dads.de</h1>
+      <h2>Welcome to the coding dads webpage</h2>
+    </div>
+  );
+}
